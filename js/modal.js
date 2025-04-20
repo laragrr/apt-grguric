fef5f87891ab650f57e5434df1e386cea9a2d09d
LaraG <lara.grguric@fer.hr>
2025-04-20 22:27:33 +0200

    Add fade in animation

---
+++ js/modal.js
@@ -0,0 +1,33 @@
+// js/modal.js
+const imageGroups = {
+   'A': ['images/apt-A.jpg', 'images/apt-A-2.jpg', 'images/apt-A-3.jpg'],
+   'B': ['images/apt-B.jpg', 'images/apt-B-2.jpg'],
+   'C': ['images/studio.jpg', 'images/studio-2.jpg']
+};
+
+let currentSlide = 0;
+let currentImages = [];
+
+function openGallery(unit) {
+   currentImages = imageGroups[unit];
+   const container = document.getElementById("modalImagesContainer");
+   container.innerHTML = currentImages.map((src, i) =>
+     `<img src="${src}" class="${i === 0 ? 'active' : ''}">`
+   ).join('');
+   currentSlide = 0;
+   const modal = document.getElementById("galleryModal");
+   modal.classList.add("show");
+}
+
+function closeModal() {
+   const modal = document.getElementById("galleryModal");
+   modal.classList.remove("show");
+}
+
+function changeSlide(n) {
+   const images = document.querySelectorAll("#modalImagesContainer img");
+   images[currentSlide].classList.remove("active");
+   currentSlide = (currentSlide + n + images.length) % images.length;
+   images[currentSlide].classList.add("active");
+}
+ 
